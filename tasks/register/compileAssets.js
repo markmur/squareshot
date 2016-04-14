231232d5cbe60f50d9651b3b2c2f54047b2b17c8
Mark Murray <mmmarkmurray@gmail.com>
2016-04-14 10:31:17 +0100

    adding postinstall back

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -6,13 +6,13 @@ var compileAssets = [
     'copy:dev',
 ];
 
-console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'development') {
   compileAssets[compileAssets.length] = 'exec:webpackDevServer';
-} else {
-  compileAssets[compileAssets.length] = 'exec:webpackBuild';
 }
+
+// else {
+//   compileAssets[compileAssets.length] = 'exec:webpackBuild';
+// }
 
 module.exports = function (grunt) {
   grunt.registerTask('compileAssets', compileAssets);
