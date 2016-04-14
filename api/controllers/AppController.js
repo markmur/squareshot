99dcdfcb78d39e8cea7c2a6a27a3c1cad1c4909a
Mark Murray <mmmarkmurray@gmail.com>
2016-04-14 11:29:28 +0100

    console log __dirname

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -1,5 +1,8 @@
 module.exports = {
   index: function (req, res) {
+
+    console.log(__dirname);
+
     return res.view('app/index.ejs', {
       bundle: require('../../assets.json').main.js,
       user: req.session.user,
